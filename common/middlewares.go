ec3c2848dd96ada7ba968d67ad04bf80e44f73e4
hwangseonu <hwangseonu12@naver.com>
2019-02-01 20:55:52 +0900

    feat: Add middleware

---
+++ common/middlewares.go
@@ -0,0 +1,40 @@
+package common
+
+import (
+	"log"
+	"net/http"
+)
+
+type Middleware func(next http.Handler) http.HandlerFunc
+
+func ChainBeforeMiddlewares(middlewares ...Middleware) Middleware {
+	return func(final http.Handler) http.HandlerFunc {
+		return func(res http.ResponseWriter, req *http.Request) {
+			var last http.HandlerFunc = final.ServeHTTP
+			for i := len(middlewares) - 1; i >= 0; i-- {
+				last = middlewares[i](last)
+			}
+			last.ServeHTTP(res, req)
+		}
+	}
+}
+
+func ChainAfterMiddlewares(middlewares ...Middleware) Middleware {
+	return func(final http.Handler) http.HandlerFunc {
+		return func(res http.ResponseWriter, req *http.Request) {
+			final.ServeHTTP(res, req)
+			var last http.HandlerFunc = func(res http.ResponseWriter, req *http.Request) {}
+			for i := len(middlewares) - 1; i >= 0; i-- {
+				last = middlewares[i](last)
+			}
+			last.ServeHTTP(res, req)
+		}
+	}
+}
+
+func LoggerMiddleware(next http.Handler) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		log.Printf("[%s] %s", req.Method, req.URL.Path)
+		next.ServeHTTP(res, req)
+	}
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/hwangseonu/goBackend/common"
 	"github.com/hwangseonu/goBackend/users"
 	"net/http"
 )
@@ -8,8 +9,12 @@ import (
 const port = ":5000"
 
 func main() {
-	http.Handle("/auth", new(users.AuthController))
-	http.Handle("/users", new(users.UserController))
+	before := common.ChainBeforeMiddlewares(common.LoggerMiddleware,)
+	after := common.ChainAfterMiddlewares(common.LoggerMiddleware)
+
+	http.HandleFunc("/auth", before(after(new(users.AuthController))))
+	http.Handle("/users", before(after(new(users.UserController))))
+
 	println("server is running on port " + port)
 	err := http.ListenAndServe(port, nil)
 	if err != nil {
